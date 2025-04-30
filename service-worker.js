605359a249d57ad2274116e99869bd303a117878
Targia94 <giacomo.tarantino94@gmail.com>
2025-04-30 13:34:31 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,24 @@
-const cacheName = "mini-pwa-v1";
-const assets = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/app.js",
-  "/manifest.json"
+const CACHE_NAME = "dashboard-pwa-v1";
+const ASSETS = [
+  "/dashboard.html",
+  "/manifest.json",
+  "/service-worker.js",
+  "/icon.png",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(cacheName).then((cache) => cache.addAll(assets))
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
